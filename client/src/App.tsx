2a5a6350bad4fc9f5d72bc0bbe77020178a9bc72
DURNERIN NATHAN <durnerin.nathan01@gmail.com>
2025-10-19 13:33:14 +0200

    fix rename

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 //Update Header
-import "./app.css";
+import "./App.css";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
