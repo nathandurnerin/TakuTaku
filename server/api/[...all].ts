98cae6cd25f60983f08e4b1a78e4ce158dcb10bf
DURNERIN NATHAN <durnerin.nathan01@gmail.com>
2025-10-19 19:11:39 +0200

    fix 11

--- server/api/[...all].ts
+++ server/api/[...all].ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import app from '../server/src/app';
+import app from '../src/app';
 
 // Délègue absolument toutes les routes /api/** à Express
 export default (req: VercelRequest, res: VercelResponse) => (app as any)(req, res);
